cf132b594db400996808552e135f29ce0f8be375
Rheinmetals <100020349+Rheinmetals@users.noreply.github.com>
2025-05-28 02:09:36 +0800

    全局类化，版本控制，修复问题。程序当前版本 1.0

--- Thingsdatabase.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Thingstable` (
-  `title` VARCHAR(100) NOT NULL,
-  `deadline` VARCHAR(45) NOT NULL,
-  `text` VARCHAR(400) NOT NULL,
-  `id` INT NOT NULL,
-  `level` INT NOT NULL,
-  `isfinished` TINYINT NOT NULL,
-  PRIMARY KEY (`id`))
